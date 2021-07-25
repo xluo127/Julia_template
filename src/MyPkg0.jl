fb485b8a55737009ea995aab8a1d111a82702e3d
Xiaotian Luo <1521338458@qq.com>
2021-07-25 16:20:16 +0800

    Update MyPkg0.jl
    test for sog()

--- src/MyPkg0.jl
+++ src/MyPkg0.jl
@@ -3,11 +3,26 @@
 # Write your package code here.
 module MyPkg0
 
-    export test1
+    export sog0
 
-    function test1(x,y,z)
-        x*y*z
-
+    function sog0(x...)
+        iVector = x[1]                        
+        len = length(iVector)
+        if len==0
+            return nothing
+        end
+    
+        temp = iVector[1]
+        if len==1
+            return [true]
+        end
+    
+        oVector = Vector{Bool}(undef, len)
+        oVector[1] = Bool(1)
+        for i in 2:len
+            oVector[i] = ifelse(isequal(iVector[i], iVector[i-1]), false, true)
+        end
+        return oVector
     end
 end
 
