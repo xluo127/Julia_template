c1c4b78ba646febd42c7a077b99b4a69b3d8640d
Xiaotian Luo <1521338458@qq.com>
2021-07-27 10:40:20 +0800

    final2

--- src/MyPkg0.jl
+++ src/MyPkg0.jl
@@ -3,18 +3,18 @@ module MyPkg0
 
     export sog
     """
-        sog(x...)
+        sog(x)
 
-    The function sog() takes a `Vector` of any type `x...` as its input with any possible length, and will return a `Bool Vector` with the same length as input. Values
+    The function sog() takes a `Vector` of any type `x` as its input with any possible length (default is Any[]), and will return a `Bool Vector` with the same length as input. Values
      `true` or `false` in the output depends on whether one element in the `Vector` is the start of a group or not. The sog() will return 
      'nothing' if the length of input is 0. As any type can be applied in the input, be careful! To check whether two elements `a` and `b` are treated
     as the same, please call `isequal(a, b)` to see the result. 
 
     Examples:
 
-        sog([1,2,2,2,1,3,3,1,1]) = [true, true, false, false, true, true, false, true, false]
-        sog() = nothing
-        sog(["a", "a", "a", 'a']) = [true, false, false, true]
+        sog([1,2,2,2,1,3,3,1,1]) returns: [true, true, false, false, true, true, false, true, false]
+        sog() returns: nothing
+        sog(["a", "a", "a", 'a']) returns: [true, false, false, true]
 
     """
     function sog(x=[])                       
